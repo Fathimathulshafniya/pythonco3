5999e9e62892a38bd8115f252eef38c5423eb476
fathimathulshafniya <shafniyamusthafa@gmail.com>
2021-03-23 23:00:30 +0530

    commit31

---
+++ 1builtinpackgs.py
@@ -0,0 +1,6 @@
+import math
+
+num = int(input("Enter a number: "))
+p = int(input("Enter the power: "))
+print(num,"to the power of", p, " is: ", math.pow(num, p))
+print("Square root of ", num, " is: ", math.sqrt(num))
